df9ce14b9933c768209280a714dd665f1c756940
@sensplastik <sensplastik@gmail.com>
2024-10-03 22:04:26 +0100

    update config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import vue from '@astrojs/vue';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://sensplastik.github.io',
+  //base: 'static.sensplastik.studio',
+
   integrations: [vue({ devtools: true , appEntrypoint: '/src/app' })],
   vite:{
     css: {
